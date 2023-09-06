ce869eae20f7c9a67de85681b99e12661c5cd45c
Ragu <yourragu@gmail.com>
2023-09-06 16:56:59 +0530

    Add Health Check file

---
+++ Health_check_Ragu.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+Threshold_mem=15
+Threshold_cpu=70
+Threshold_dsk=40
+
+#Condition checl
+bPassed=true
+
+#Memory Check
+mem_value=$(free | grep Mem |awk '{print int($3/$2*100)}')
+echo 'Memory value is:'$mem_value
+
+if((mem_value>Threshold_mem));then
+echo "Running out of memory"
+bPassed=false
+fi
+
+#CPU Check
+cpu_mem=$(top -n1| grep Cpu | awk '{print int($2)}')
+echo 'CPU value is :'$cpu_mem
+if((cpu_mem>Threshold_cpu));then
+echo 'CPU is running out of memory'
+bPassed=false
+fi
+
+#Disk Check
+#disk_mem=df | grep dev/root | awk '{gsub("%","");print $5}'
+disk_mem=$(df / | tail -1 |awk  '{gsub("%","");print $5}')
+echo 'Disk Mem is'$disk_mem
+if((disk_mem>Threshold_dsk));then
+echo 'Running ou of disk space! Abort execution'
+bPassed=false
+fi
+
+if($bPassed);then
+echo 'Health check passed....Execution started..'
+bash testAutomatedScript.sh
+else
+echo 'Health check failed....Execution aborted..Re-booting the instance..please wait'
+aws ec2 reboot-instances --instance-ids i-08e65b1d0b4ca26ce 
+fi
+
+
+
